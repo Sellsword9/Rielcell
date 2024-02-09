8eb3b251a5fcd3f59ceab6363cae8788949319ed
Yeray Romero <yerayromerodev@gmail.com>
2024-02-09 14:03:17 +0100

    Doing some testing

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,13 @@
-use mysql::*;
+use std::collections::HashMap;
+use std::fs;
+use std::hash::Hasher;
+
 use actix_web::*;
 use actix_web::cookie::Cookie;
 use actix_web::web::{Data, Form};
 use actix_files::*;
+use mysql::prelude::Queryable;
+use mysql::{params, Pool, PooledConn};
 use serde::{Serialize, Deserialize};
 mod db;
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -18,17 +23,46 @@ async fn main() -> std::io::Result<()> {
     if pool.is_none() {
         panic!("Failed to create pool");
     }
-    let addrs = "localhost:8000";
+    let addrs = "localhost:4000";
     HttpServer::new(move || {
         App::new()
-            .app_data(pool.clone())
+            .app_data(Data::new(pool.clone().unwrap()))
             .service(login)
             .service(logout)
+            .service(index)
     }).bind(addrs)?
         .run()
         .await
 }
 
+
+#[post("/login")]
+async fn login(req: HttpRequest) -> HttpResponse {
+    let form = Form::<HashMap<String, String>>::extract(&req).await.unwrap();
+    let username = form.get("username").unwrap();
+    let password = form.get("password").unwrap();
+    let pool = req.app_data::<Pool>().unwrap();
+    let pool: Pool = pool.clone();
+    let logged = login_user(pool, username, password).await;
+    return HttpResponse::Ok().body("Testing");
+    /*if logged.is_some() {
+        HttpResponse::Ok()
+            .cookie(Cookie::build("user_id", ).path("/").finish())
+            .finish()
+        HttpResponse::Ok().finish()
+    } else {
+        HttpResponse::Unauthorized().finish()
+    }*/
+}
+#[get("/")]
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("view/index.html")?)
+}
+
+async fn login_user(pool: Pool, username: &str, password: &str) -> Option<User> {
+    let mut conn: PooledConn = pool.get_conn().unwrap();
+    return None;
+}
 #[get("/logout")]
 async fn logout(req: HttpRequest) -> HttpResponse {
     if let Some(_) = req.cookie("user_id") {
@@ -38,8 +72,4 @@ async fn logout(req: HttpRequest) -> HttpResponse {
     } else {
         HttpResponse::Unauthorized().finish()
     }
-}
-#[post("/login")]
-async fn login(form: Form<User>, db: Data<Pool>) -> HttpResponse {
-    HttpResponse::Unauthorized().finish()
 }
